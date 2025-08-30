91729743cf2f5b3b44d54299d17b4de6100ba7b3
Pascal Rainville <pascal.rainville@gmail.com>
2025-08-30 10:15:37 -0400

    .

---
+++ src/route/file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const {error, mimeType} = require('../utils')
+
+module.exports = (req, res, baseDir, filePath) => {
+
+  // Prevent directory traversal attacks
+  if (!filePath.startsWith(baseDir)) {
+    return error(res, 403, 'Access denied', filePath)
+  }
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      return error(res, 404, 'File not found', filePath)
+    }
+
+    if (stats.isDirectory()) {
+      filePath = path.join(filePath, 'index.html');
+    }
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        console.error("Server error", filePath)
+        return error(res, 500, 'Server error', filePath)
+      }
+
+      res.setHeader('X-Content-Type-Options', 'nosniff');
+      //res.setHeader('X-Frame-Options', 'DENY'); I can't do this security, I am using an iframe.
+      //res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'"); TODO: Understand this. It breaks if enabled.
+
+      res.writeHead(200, {'Content-Type': mimeType(filePath)});
+      res.end(content);
+    });
+  });
+}
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
 const path = require('path');
 
 const handleMediaTree = require('./route/media_tree')
 const handleMediaList = require('./route/media_list')
 const handleThumbnail = require('./route/thumb')
+const handleFile = require('./route/file')
 
-const {error, mimeType} = require('./utils')
+const {error} = require('./utils')
 const config = require('./config');
 
 const FILES_DIR = config.directory_files
@@ -28,45 +28,14 @@ module.exports = async (req, res) => {
       return await handleThumbnail(req, res)
     }
 
-    let baseDir = ''
-    let filePath = ''
     if (pathname.startsWith('/files')) {
-      baseDir = FILES_DIR
-      filePath = path.join(baseDir, pathname.slice(6));
+      return handleFile(req, res, FILES_DIR, pathname.slice(6))
     } else {
-      baseDir = docsDir
       // Default to index.html if root is requested
-      filePath = path.join(baseDir, req.url === '/' ? 'index.html' : req.url);
+      return handleFile(req, res, docsDir, path.join(docsDir, req.url === '/' ? 'index.html' : req.url))
     }
 
-    // Prevent directory traversal attacks
-    if (!filePath.startsWith(baseDir)) {
-      return error(res, 403, 'Access denied', filePath)
-    }
-
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        return error(res, 404, 'File not found', filePath)
-      }
-
-      if (stats.isDirectory()) {
-        filePath = path.join(filePath, 'index.html');
-      }
-
-      fs.readFile(filePath, (err, content) => {
-        if (err) {
-          console.error("Server error", filePath)
-          return error(res, 500, 'Server error', filePath)
-        }
-
-        res.setHeader('X-Content-Type-Options', 'nosniff');
-        //res.setHeader('X-Frame-Options', 'DENY'); I can't do this security, I am using an iframe.
-        //res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'"); TODO: Understand this. It breaks if enabled.
-
-        res.writeHead(200, {'Content-Type': mimeType(filePath)});
-        res.end(content);
-      });
-    });
+    return error(res, 404, 'Not found', req.url)
   } catch (err) {
     return error(res, 500, 'Server Error', err)
   }
